3abd336806097aefed24a60bd568f87d57983db1
h4sh3 <h4sh3@git.com>
2020-10-16 12:32:18 +0200

    change visualizations

--- visualize.js
+++ visualize.js
@@ -1,27 +1,43 @@
 function drawAgent(agent) {
-  fill(0,255,0)
+  fill(0, 255, 0)
 
   push()
-  translate(agent.pos.x - agent.size.x / 2, agent.pos.y - agent.size.y / 2)
-  //rotate(agent.vel.heading()-90)
-  rect(0,0, agent.size.x, agent.size.y)
+  translate(agent.pos.x, agent.pos.y)
+  rotate(agent.vel.heading() + 90)
+  rect(0, 0, agent.size.x, agent.size.y)
   pop()
 
   agent.sensors.forEach(s => {
     noFill()
-    ellipse(s.x, s.y, 5, 5)
+    const current = s.pos.copy().rotate(s.rot)
+    current.rotate(agent.vel.heading())
+    current.add(agent.pos)
+    ellipse(current.x, current.y, 5, 5)
   })
+
 }
 
-function drawBuilding(b) {
-  stroke(0, 0, 25)
-  noFill()
+function drawBlock(b) {
   rect(b.pos.x, b.pos.y, b.size.x, b.size.y)
 }
 
-function drawCity(c) {
-  c.buildings.forEach(b => {
-    drawBuilding(b)
+function drawEnvironment(c) {
+  drawBuildings(c.buildings)
+  drawCheckPoints(c.checkPoints)
+}
+
+function drawCheckPoints(checkPoints) {
+  fill(0, 255, 0)
+  checkPoints.forEach(b => {
+    drawBlock(b)
+  })
+}
+
+function drawBuildings(buildings) {
+  stroke(0, 0, 25)
+  noFill()
+  buildings.forEach(b => {
+    drawBlock(b)
   })
 }
 
