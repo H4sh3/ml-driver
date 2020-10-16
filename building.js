e3dd78ea5a42153e2d095925391fd8f4d8553d3b
h4sh3 <h4sh3@git.com>
2020-10-16 12:31:48 +0200

    draw collisions

--- building.js
+++ building.js
@@ -5,6 +5,11 @@ class Building {
   }
 
   colliding(p) {
-    return p.x > this.pos.x && p.y > this.pos.y && p.x < this.pos.x + this.size.x && p.y < this.pos.y + this.size.y
+    const coliding = p.x > this.pos.x && p.y > this.pos.y && p.x < this.pos.x + this.size.x && p.y < this.pos.y + this.size.y
+    if(coliding){
+      fill(255,0,0)
+      ellipse(p.x,p.y,5,5)
+    }
+    return coliding
   }
 }
