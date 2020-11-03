b5face73ffc1b46d2e0b7df395f663493ee0ba98
h4sh3 <h4sh3@git.com>
2020-11-03 22:43:09 +0100

    15 episodes ofc

--- configurations/src/components/Description.js
+++ configurations/src/components/Description.js
@@ -5,8 +5,8 @@ export default function Description(){
   Hover over cells to see the Drivers configuration.
   Click to run it in the environment.<br />
     <br />
-  Green: Agent solved environment in under 30 episodes.<br />
-  Black: Agent didn't solve environment in 30 episodes.<br />
+  Green: Agent solved environment in under 15 episodes.<br />
+  Black: Agent didn't solve environment in 15 episodes.<br />
   Grey: Untested configurations.
   </div>
   )
