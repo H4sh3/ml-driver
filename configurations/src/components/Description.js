bd6f57a9ed920015c0a3524fcb7e5171162a3937
h4sh3 <pinkdaniel@web.de>
2020-11-14 09:52:33 +0100

    style

--- configurations/src/components/Description.js
+++ configurations/src/components/Description.js
@@ -1,13 +1,12 @@
 export default function Description(){
   return (
     <div>
-    This grid represents all configurations for the ML-Drivers.<br />
-  Hover over cells to see the Drivers configuration.
-  Click to run it in the environment.<br />
-    <br />
-  Green: Agent solved environment in under 15 episodes.<br />
-  Black: Agent didn't solve environment in 15 episodes.<br />
-  Grey: Untested configurations.
-  </div>
+      This grid represents all configurations for the ML-Drivers.<br />
+      Hover over cells to see the Drivers configuration. Click to run it in the environment.<br />
+      <br />
+      Green: Agent solved environment in under 15 episodes.<br />
+      Black: Agent didn't solve environment in 15 episodes.<br />
+      Grey: Untested configurations.
+    </div>
   )
 }
